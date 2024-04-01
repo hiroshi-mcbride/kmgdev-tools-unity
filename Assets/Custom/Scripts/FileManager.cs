a6380cb2ad507a39852b5dfa342138ab465648b7
Michael Yamada <roninnarasu@gmail.com>
2024-04-01 16:41:37 +0200

    load file from browser

--- Assets/Custom/Scripts/FileManager.cs
+++ Assets/Custom/Scripts/FileManager.cs
@@ -1,24 +1,68 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using SimpleFileBrowser;
 
 public class FileManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private event FileBrowser.OnSuccess onLoadSuccess;
+    private event FileBrowser.OnSuccess onSaveSuccess;
+
+    private event FileBrowser.OnCancel onCancel;
+    
+
+    public void OnLoad()
+    { 
+        FileBrowser.ShowLoadDialog(onLoadSuccess, onCancel, FileBrowser.PickMode.Files);
+    }
+    
+    public void OnSave()
+    {
+        FileBrowser.ShowSaveDialog(onSaveSuccess, onCancel, FileBrowser.PickMode.Files);
+    }
+
+    private void ReadFile(string[] _paths)
+    {
+        foreach (string path in _paths)
+        {
+            if (File.Exists(path))
+            {
+                Debug.Log(File.ReadAllText(path));
+            }
+        }
+    }
+
+    private void WriteFile(string[] _paths)
     {
         
     }
 
-    public void OnLoad()
-    { 
-        //FileBrowser.ShowLoadDialog()
+    private void OnLoadCanceled()
+    {
+        
     }
-
+    
+    private void Start()
+    {
+        FileBrowser.SetFilters(false, new FileBrowser.Filter("", ".mfsp"));
+        
+        // HideDialog initializes the file browser, preventing lag when opening the dialog
+        FileBrowser.HideDialog();
+    }
+    
     private void OnEnable()
     {
-        //FileBrowser.OnSuccess += 
+        onLoadSuccess += ReadFile;
+        onSaveSuccess += WriteFile;
+        onCancel += OnLoadCanceled;
+    }
+    
+    private void OnDisable()
+    {
+        onLoadSuccess -= ReadFile;
+        onSaveSuccess -= WriteFile;
+        onCancel -= OnLoadCanceled;
     }
 }
